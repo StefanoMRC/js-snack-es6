4f3ce6e6bd3240c307b425927a3f91ed8b7d9578
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-25 14:02:41 +0100

    fine snack1

---
+++ Snack1/main.js
@@ -0,0 +1,43 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+let bici=[
+   
+    {
+        'nome':'va-lentino',
+        'peso':'2550gr'
+    },
+    {
+        'nome':'si poteva fare meglio',
+        'peso':'1950gr'
+    },
+    {
+        'nome':'cosa con due ruote',
+        'peso':'1740gr'
+    },
+    {
+        'nome':'velociraptor',
+        'peso':'850gr'
+    }
+
+]
+
+let stampa=document.getElementById('cont')
+let stampa2=document.getElementById('bici')
+
+let pesoMinore =bici[0]
+for(i=0; i<bici.length; i++){
+    stampa2.innerHTML+=`<p>${[i+1]}) ${bici[i].nome} ${bici[i].peso}</p>`
+    let x= bici[i]
+    if(pesoMinore['peso']<x['peso']){
+        pesoMinore=x
+    }
+    
+}
+console.log(pesoMinore)
+
+stampa.innerHTML=`
+la bici col peso minore è ${pesoMinore.nome} col peso di ${pesoMinore.peso}
+`
