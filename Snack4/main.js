564d540af24cbad6fb88d7cdee2104dbc63e6075
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-29 10:06:17 +0200

    aggiunti commenti

--- Snack4/main.js
+++ Snack4/main.js
@@ -17,7 +17,7 @@
      { name: 'Paola', id: 110, grades: 96 },
      { name: 'Andrea', id: 250, grades: 48 },
      { name: 'Gaia', id: 145, grades: 74 },
-     { name: 'Luigi', id: 196, grades: 68 },
+     { name: 'Luigi', id: 196, grades: 68 },          
      { name: 'Piero', id: 102, grades: 50 },
      { name: 'Francesca', id: 120, grades: 84 },
    ];
@@ -27,9 +27,8 @@
    let stampa4=document.getElementById("cont4")
    let stampa5=document.getElementById("cont5")
 
-//    students.forEach((element,index) => {
-//     console.log(element.name)
-//    });
+// 1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array sarà il nome dello studente ma con tutte le lettere maiuscole.   
+
 let arrayNomi= students.map(
     (element)=>{
         return element.name.toUpperCase()
@@ -41,6 +40,7 @@ stampa1.innerHTML=
 `
 <p>La stampa di tutti i nomi in maiuscolo ${arrayNomi}</p>
 `
+// Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70
 
 let arrayVoti=students.filter(
     (element)=>element.grades>70
@@ -52,6 +52,7 @@ arrayVoti.forEach((element, index) => {
     stampa3.innerHTML+=`<li>${arrayVoti[index].name} con voto ${arrayVoti[index].grades}</li> `
 });
 
+// 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 
 let arrayVoti2=students.filter(
     (element)=>element.id>120 && element.grades>70
